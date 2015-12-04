b0d1ebb3467e598f635f6615748e0c250b77908c
Lenovo <Lenovo@10.50.130.216>
2015-12-04 15:58:02 +0100

    Started on, search engine Algorithm 2.

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -2,15 +2,30 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view1', {
+                    templateUrl: 'app/view1/view1.html',
+                    controller: 'View1Ctrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-.controller('View1Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+        .controller('View1Ctrl', ['$http', function ($http) {
+//  this.msgFromFactory = InfoFactory.getInfo();
+//  this.msgFromService = InfoService.getInfo();
+
+                $http.get('api/flightinfo/CPH/2016-01-04T23:00:00.000Z/3')
+                        .success(function (data, status, headers, config) {
+                            //data = data;
+                            
+                            console.log(data);
+                            
+                            //$scope.msg = "Du er nu logget ind som USER";
+                            //$scope.ifUser = true;
+
+                        })
+                        .error(function (data, status, headers, config) {
+
+                        });
+
+            }]);

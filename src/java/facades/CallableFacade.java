4dadb985157a79b340cd0bb58af6758f37e45a89
ismlcm <ismlcam@me.com>
2015-12-08 10:47:45 +0100

    CallableFacade videre.. Ismail

--- src/java/facades/CallableFacade.java
+++ src/java/facades/CallableFacade.java
@@ -23,29 +23,34 @@ import java.util.concurrent.Future;
  *
  * @author AlexanderNielsen
  */
-public final class CallableFacade implements Callable<String> {
+public class CallableFacade implements Callable<String>
+{
+
     private String url;
     private String from;
     private String to;
     private String date;
     private String persons;
-    
-    public CallableFacade(String url, String from, String to, String date, String persons) {
+
+    public CallableFacade( String url, String from, String to, String date, String persons )
+    {
         this.url = url;
         this.from = from;
         this.to = to;
         this.date = date;
         this.persons = persons;
-        runThreads(from, to, date, persons);
     }
 
     @Override
-    public String call() throws Exception {
-        String status = getStatus();
+    public String call() throws Exception
+    {
+        String status = getFromUrl();
         return url + "\t:\t " + status + "\t:\t" + Thread.currentThread().getName();
     }
 
-    static String[] hostList = {"http://angularairline-plaul.rhcloud.com/api/flightinfo/", 
+    static String[] hostList =
+    {
+        "http://angularairline-plaul.rhcloud.com/api/flightinfo/",
         "http://angularairline-plaul.rhcloud.com/api/flightinfo/",
         "http://angularairline-plaul.rhcloud.com/api/flightinfo/",
         "http://angularairline-plaul.rhcloud.com/api/flightinfo/",
@@ -54,73 +59,84 @@ public final class CallableFacade implements Callable<String> {
         "http://angularairline-plaul.rhcloud.com/api/flightinfo/"
     };
 
-    public void runThreads(String from, String to, String date, String persons){
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+    public void runThreads()
+    {
+        ExecutorService executor = Executors.newFixedThreadPool( 4 );
         List<Future<String>> list = new ArrayList<>();
-        
-        for (int i = 0; i < hostList.length; i++) {
+
+        for ( int i = 0; i < hostList.length; i++ )
+        {
             String url = hostList[i];
-            Callable<String> callable = new CallableFacade(url, from, to, date, persons);
-            Future<String> future = executor.submit(callable);
-            list.add(future);
+            Callable<String> callable = new CallableFacade( url, from, to, date, persons );
+            Future<String> future = executor.submit( callable );
+            list.add( future );
         }
-        for(Future<String> fut : list){
-            try {
+        for ( Future<String> fut : list )
+        {
+            try
+            {
                 //print the return value of Future, notice the output delay in console
                 // because Future.get() waits for task to get completed
-                System.out.println(fut.get());
-            } catch (InterruptedException | ExecutionException e) {
+                System.out.println( fut.get() );
+            }
+            catch (InterruptedException | ExecutionException e)
+            {
                 e.printStackTrace();
             }
         }
-        
-        
-        
+
     }
-    
-    public String getStatus() throws IOException {
-        URL url1 = new URL(url + from + "/" + to + "/" + date + "/" + persons);
-        
-        if (to == null || to == "")
+
+    public String getFromUrl() throws IOException
+    {
+        URL url1 = new URL( url + from + "/" + to + "/" + date + "/" + persons );
+
+        if ( to == null || to == "" )
         {
-            url1 = new URL(url + from + "/" + date + "/" + persons);
+            url1 = new URL( url + from + "/" + date + "/" + persons );
         }
-        
-            String output = "";
-            String output2 = "";
-        try {
+
+        String output = "";
+        String output2 = "";
+        try
+        {
 
             HttpURLConnection conn = (HttpURLConnection) url1.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestMethod( "GET" );
+            conn.setRequestProperty( "Accept", "application/json" );
 
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
+            if ( conn.getResponseCode() != 200 )
+            {
+                throw new RuntimeException( "Failed : HTTP error code : "
+                        + conn.getResponseCode() );
             }
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
+            BufferedReader br = new BufferedReader( new InputStreamReader(
+                    (conn.getInputStream()) ) );
 
-           // System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
+            // System.out.println("Output from Server .... \n");
+            while ( (output = br.readLine()) != null )
+            {
                 //System.out.println(output);
                 output2 += output;
             }
 
             conn.disconnect();
 
-        } catch (MalformedURLException e) {
+        }
+        catch (MalformedURLException e)
+        {
             //System.err.println(e.getMessage());
-           e.printStackTrace();
+            e.printStackTrace();
 
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
 
             e.printStackTrace();
 
         }
         return output2;
     }
-    
-    
+
 }
--- src/java/tester/mainTest.java
+++ src/java/tester/mainTest.java
@@ -15,5 +15,7 @@ public class mainTest {
     public static void main(String[] args) throws Exception {
         CallableFacade cf = new CallableFacade("", "CPH", "SXF", "2016-01-01T00:00:00.000Z", "2");
         
+        cf.runThreads();
+        
     }
 }

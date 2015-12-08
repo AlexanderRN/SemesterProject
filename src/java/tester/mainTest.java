155afd372013f3befce34246232d58173742f558
AlexanderRN <AlexanderRNielsen@hotmail.dk>
2015-12-08 10:06:23 +0100

    Threadpool added - works, need polishing.

---
+++ src/java/tester/mainTest.java
@@ -0,0 +1,19 @@
+
+package tester;
+
+import facades.CallableFacade;
+
+
+
+/**
+ *
+ * @author AlexanderNielsen
+ */
+public class mainTest {
+
+    
+    public static void main(String[] args) throws Exception {
+        CallableFacade cf = new CallableFacade("", "CPH", "SXF", "2016-01-01T00:00:00.000Z", "2");
+        
+    }
+}
